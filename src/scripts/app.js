b32d3e7ef0378c38ed268050675bd9a51b3dfea8
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-11-02 15:12:01 +0100

    refactor: :recycle: Rewrote code in a more clearly way

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,53 +1,48 @@
-const requestContent = (fileName) => {
-  let url = `../routes/${fileName}.html`;
-  let req = new XMLHttpRequest();
-  req.open("GET", url, false);
-  req.send()
-  return req.responseText;
-}
-  
-const routes = {
-  "/": requestContent("home"),
-  "/about": requestContent("about"),
-  "/projects": requestContent("projects"),
-  "/photos": requestContent("photos"),
-};
-
-const rootDiv = document.getElementById("root");
-if (!routes[window.location.pathname]) {
-  rootDiv.innerHTML = "404 This page can't be found";
-} else {
-  rootDiv.innerHTML = routes[window.location.pathname];
-}
-
-const navigate = (e, pathname) => {
-  e.preventDefault();
-  window.history.pushState({}, pathname, window.location.origin + pathname);
-  rootDiv.innerHTML = routes[pathname];
-  changeName();
-};
-
-window.onpopstate = () => {
-  rootDiv.innerHTML = routes[window.location.pathname];
-};
-
+const root = document.getElementById("root");
+const home = document.getElementById("home");
 const about = document.getElementById("about");
 const photos = document.getElementById("photos");
 const projects = document.getElementById("projects");
 
-home.onclick = (e) => navigate(e, "/");
-about.onclick = (e) => navigate(e, "/about");
-photos.onclick = (e) => navigate(e, "/photos");
-projects.onclick = (e) => navigate(e, "/projects");
+home.onclick = (e) => {
+  console.log("Home clicked");
+  e.preventDefault();
+  navigate("/");
+};
+about.onclick = (e) => {
+  console.log("about clicked");
+  e.preventDefault();
+  navigate("/about");
+};
+photos.onclick = (e) => {
+  e.preventDefault();
+  navigate("/photos");
+};
+projects.onclick = (e) => {
+  e.preventDefault();
+  navigate("/projects");
+};
 
-const changeName = () => {
-  let pathname = window.location.pathname;
+const navigate = (pathname) => {
+  let pagename = pathname.slice(1);
+  if (pathname === "/") pagename = "home";
+  window.history.pushState({}, pathname, window.location.origin + pathname);
+  // Request content
+  let url = `../routes/${pagename}.html`;
+  let req = new XMLHttpRequest();
+  req.open("GET", url, false);
+  req.send();
+  root.innerHTML = req.responseText;;
+  // Change naming
   document.getElementById("breadcrumb").innerHTML = pathname;
-  if (pathname == "/") pathname = "/home";
   document.title =
-    pathname.slice(1).charAt(0).toUpperCase() +
-    pathname.slice(2) +
+    pagename.charAt(0).toUpperCase() +
+    pagename.slice(1) +
     " - Made by Jesper";
 };
 
-changeName();
+window.onpopstate = () => {
+  root.innerHTML = navigate(window.location.pathname);
+};
+
+navigate(window.location.pathname)
