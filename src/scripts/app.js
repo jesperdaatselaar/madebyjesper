d2ee2f0ff241d8dcad889c7224d396e0544d5762
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-11-03 12:30:23 +0100

    feat: :construction: Added routing for individual photos
    Added routing based of the data in json. Still one TODO open

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,16 +15,59 @@ about.onclick = (e) => {
 photos.onclick = (e) => {
   e.preventDefault();
   navigate("/photos");
-  getPhotos();
 };
 projects.onclick = (e) => {
   e.preventDefault();
   navigate("/projects");
-  getProjects();
+};
+
+const getPhotos = () => {
+  let container = document.getElementById("photos-container");
+  // Get all photos
+  let json = new XMLHttpRequest();
+  json.open("GET", "../data/photos.json", false);
+  json.send();
+  let data = JSON.parse(json.responseText);
+  // Show all photos
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    let div = document.createElement("div");
+    div.classList.add("photo")
+    div.dataset.id = data[i].id;
+
+    let title = document.createElement("h2");
+    title.innerText = data[i].name;
+
+    let description = document.createElement("p")
+    description.classList.add("description")
+    description.innerText = data[i].description;
+
+    let image = document.createElement("img");
+    image.src = data[i].path
+    
+    div.append(title, description, image)
+    container.appendChild(div);
+    // Add event listeners
+    div.addEventListener("click", (e) => {
+      let id = e.target.parentNode.dataset.id
+      navigate(`/photos/${id}`);
+    })
+  }
+};
+
+const getProjects = () => {
+  let container = document.getElementById("projects-container");
 };
 
 const navigate = (pathname) => {
   let pagename = pathname.slice(1);
+  // Check for id
+  if (pagename.split("/").length > 1 && pagename.split("/")[0] === "photos" ){
+    pagename = "photodetail"
+  } else if (pagename.split("/").length > 1 && pagename.split("/")[0] === "projects") {
+    pagename = "projectdetail"
+  }
+
   if (pathname === "/") pagename = "home";
   window.history.pushState({}, pathname, window.location.origin + pathname);
   // Request content
@@ -33,6 +76,9 @@ const navigate = (pathname) => {
   req.open("GET", url, false);
   req.send();
   root.innerHTML = req.responseText;
+  // Run function for projects and photos
+  if (pagename === "projects") getProjects();
+  if (pagename === "photos") getPhotos();
   // Change naming
   document.getElementById("breadcrumb").innerHTML = pathname;
   document.title =
@@ -45,10 +91,5 @@ window.onpopstate = () => {
 
 navigate(window.location.pathname);
 
-const getPhotos = () => {
-  let container = document.getElementById("photos-container");
-};
 
-const getProjects = () => {
-  let container = document.getElementById("projects-container");
-};
+// TODO: FIX HISTORY CLICK NOT SHOWING PHOTOS
