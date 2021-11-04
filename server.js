4b0c025cfa652e897579e97efa32dfe0f414b31e
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-11-04 13:41:07 +0100

    fix: :construction: Changed static folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, "src")));
+app.use("/src", express.static(path.resolve(__dirname, "src")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "src", "index.html"));
