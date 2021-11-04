8ab566057213b4b8e932f2c882065614534e8bea
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-11-04 14:43:53 +0100

    refactor: :recycle: Added every page to apropriate route
    Also formatted much nicer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,45 +1,68 @@
-const navigateTo = url => { 
+import Home from "../views/Home.js";
+import About from "../views/About.js";
+import Photos from "../views/Photos.js";
+import Projects from "../views/Projects.js";
+import FourOFour from "../views/404.js";
+
+const navigateTo = (url) => {
   history.pushState(null, null, url);
   router();
-}
+};
 
 const router = async () => {
   const routes = [
-    { path: "/", view: () => { console.log("Home")}},
-    { path: "/about", view: () => { console.log("About")}},
-    { path: "/projects", view: () => { console.log("Projects")}},
-    { path: "/photos", view: () => { console.log("Photos")}},
-    { path: "/404", view: () => { console.log("404")}}
+    {
+      path: "/",
+      view: Home,
+    },
+    {
+      path: "/about",
+      view: About,
+    },
+    {
+      path: "/photos",
+      view: Photos,
+    },
+    {
+      path: "/projects",
+      view: Projects,
+    },
+    {
+      path: "/404",
+      view: FourOFour,
+    },
   ];
 
-  const potentialMatch = routes.map(r => { 
-    return { 
+  const potentialMatch = routes.map((r) => {
+    return {
       route: r,
-      result: location.pathname === r.path
-    }
+      result: location.pathname === r.path,
+    };
   });
 
-  let match = potentialMatch.find(potentialMatch => potentialMatch.result);
+  let match = potentialMatch.find((potentialMatch) => potentialMatch.result);
 
   if (!match) {
     match = {
-      route:  routes.find(r => r.path === "/404"),
-      result: true
-    }
+      route: routes.find((r) => r.path === "/404"),
+      result: true,
+    };
   }
 
-  console.log(match)
-}
+  const view = new match.route.view();
 
-window.addEventListener('popstate', router )
+  document.querySelector("#root").innerHTML = await view.getHTML();
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.addEventListener("click", e => {
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
     if (e.target.matches("[data-link]")) {
       e.preventDefault();
-      navigateTo(e.target.href)
+      navigateTo(e.target.href);
     }
-  })
+  });
 
-  router()
-})
+  router();
+});
