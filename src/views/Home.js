e54ff0310fa9e6f7927d569440ef8fea6e5e52bc
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-11-05 17:09:27 +0100

    docs: :mag: Added meta description for home

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,6 +4,7 @@ export default class extends AbstractView {
   constructor() {
     super();
     this.setTitle("Home - Made by Jesper");
+    this.setDescription("The portfolio site of Jesper van Daatselaar");
   }
 
   async getHTML() {
