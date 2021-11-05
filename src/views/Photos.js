16c933e10b07f7a4add65350b45ab165d2186dd6
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-11-05 16:39:48 +0100

    feat: :construction: Display list of photos

--- src/views/Photos.js
+++ src/views/Photos.js
@@ -1,12 +1,19 @@
 import AbstractView from "./AbstractView.js";
-
+import photos from "../data/photos.js";
 export default class extends AbstractView {
   constructor() {
     super();
     this.setTitle("Photos - Made by Jesper");
+    this.photos = photos;
   }
 
   async getHTML() {
-    return `<h1>Photos</h1>`;
+    let output = [];
+    for (let i = 0; i < this.photos.length; i++) {
+      output.push(
+        `<a data-link href="/photos/${this.photos[i].id}"><img src="${this.photos[i].path}"></a>`,
+      );
+    }
+    return output.join("\n");
   }
 }
